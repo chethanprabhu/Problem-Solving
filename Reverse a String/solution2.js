79d347f5623f3dc2c1bb224f2606c0aaf1c8bffc
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-08-10 17:53:38 +0530

    using push and without split
    No need to use split because we can access strings in JS without converting them into array

---
+++ Reverse a String/solution2.js
@@ -0,0 +1,10 @@
+let string = "Hey, how are you?"
+//?uoy era woh .yeH
+
+let reversedArr = []; 
+
+for(let i = stringArr.length - 1; i >= 0 ; i--) {
+    reversedArr.push(stringArr[i]);
+}
+
+console.log(reversedArr.join(""));
