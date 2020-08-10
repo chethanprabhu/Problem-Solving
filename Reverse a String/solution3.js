589f9cf896afc48f838d6ad712adab8952581b34
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-08-10 17:56:56 +0530

    one line solution

--- Reverse a String/solution3.js
+++ Reverse a String/solution3.js
@@ -1,10 +1,4 @@
 let string = "Hey, how are you?"
 //?uoy era woh .yeH
 
-let reversedArr = []; 
-
-for(let i = stringArr.length - 1; i >= 0 ; i--) {
-    reversedArr.push(stringArr[i]);
-}
-
-console.log(reversedArr.join(""));
+console.log(string.split("").reverse().join(""));
