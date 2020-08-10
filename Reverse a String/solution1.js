3d8882f55207635f5e10f1da18b5dc2cbdb8abab
Chethan Prabhu <chethanprabhu65@gmail.com>
2020-08-10 17:51:19 +0530

    Common solution

---
+++ Reverse a String/solution1.js
@@ -0,0 +1,13 @@
+let string = "Hey, how are you?"
+//?uoy era woh .yeH
+
+let stringArr = string.split("");
+
+let reversedArr = []; 
+
+for(let i = stringArr.length - 1, j = 0; i >= 0 ; i--, j++) {
+    reversedArr[j] = stringArr[i];
+}
+
+console.log(reversedArr.join(""));
+
